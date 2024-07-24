1a302b2b0c9232e49bebf1ce1f0d533d5773c23d
MarcelArt <badman.islemon@gmail.com>
2024-07-24 20:57:35 +0700

    [General] Add logger

--- routes/route.go
+++ routes/route.go
@@ -4,10 +4,12 @@ import (
 	"github.com/MarcelArt/game_planner_htmx/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func SetupRoutes(app *fiber.App) {
 	app.Use(cors.New())
+	app.Use(logger.New())
 
 	app.Static("/scripts", "./views/scripts")
 	// app.Static("/src", "./views/src")
