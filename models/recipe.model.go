7df6a2e8660c69d6fef73fd6dc97e4e9bc368f3a
MarcelArt <badman.islemon@gmail.com>
2024-08-12 23:53:37 +0700

    [Recipe] Create UI for recipe list & update recipe

--- models/recipe.model.go
+++ models/recipe.model.go
@@ -10,3 +10,17 @@ type Recipe struct {
 
 	Item *Item
 }
+
+type RecipeDto struct {
+	gorm.Model
+	OutputAmount float64 `gorm:"default:1" form:"outputAmount"`
+
+	ItemID uint `gorm:"not null" form:"itemId"`
+
+	Item          *Item
+	RecipeDetails []*RecipeDetail `form:"recipeDetails"`
+}
+
+func (RecipeDto) TableName() string {
+	return "recipes"
+}
